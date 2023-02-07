d8c06f64ba9d192ed4a3b224bd56330f3a3b8c2d
Thorben157 <79640899+Thorben157@users.noreply.github.com>
2023-02-07 15:08:53 +0100

    1 file modified

--- src/app/services/show-data.service.ts
+++ src/app/services/show-data.service.ts
@@ -4,20 +4,14 @@ import { Show } from '../namensmodel/show';
 @Injectable()
 export class ShowDataService {
   shows: Show[] = [];
-  
+
   constructor() {
-    this.shows.push(new Show(1, 'Paw Patrol'))
-    this.shows.push(new Show(2, 'Seinfeld'))
-    this.shows.push(new Show(3, 'Ninjago'))
-   }
+    this.shows.push(new Show(1, 'Paw Patrol'));
+    this.shows.push(new Show(2, 'Seinfeld'));
+    this.shows.push(new Show(3, 'Ninjago'));
+  }
 
-   saveShow(show: Show) {
-
+  saveShow(show: Show) {
     this.shows.push(show);
-    
-    }
-
-    
-
-
+  }
 }
