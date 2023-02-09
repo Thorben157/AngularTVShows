078498da329ba4c463ed1c309d0600572f776339
Thorben157 <79640899+Thorben157@users.noreply.github.com>
2023-02-09 10:15:46 +0100

    2 files modified

--- src/app/components/show-list/show-list.component.ts
+++ src/app/components/show-list/show-list.component.ts
@@ -2,25 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { Show } from '../../namensmodel/show';
 import { ShowDataService } from '../../services/show-data.service';
 
-
-
 @Component({
   selector: 'app-show-list',
   templateUrl: './show-list.component.html',
-  styleUrls: ['./show-list.component.css']
+  styleUrls: ['./show-list.component.css'],
 })
 export class ShowListComponent implements OnInit {
-
-  
-  constructor(private showDataService: ShowDataService) { 
-   
-  }
+  constructor(private showDataService: ShowDataService) {}
 
   get shows(): Show[] {
     return this.showDataService.shows;
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
